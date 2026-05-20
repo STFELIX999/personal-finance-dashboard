110925edd8d0b58d9f1e33209fb22a1cdb277b92
STFELIX999 <stevin.felix9@gmail.com>
2026-05-20 12:03:10 +0530

    Initial commit feat: Created the Spring Boot project and added health check API

---
+++ src/test/java/com/stevin/personalfinancedashboard/PersonalFinanceDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevin.personalfinancedashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
