adaaf4895a7cff4c25a91668d31d6f94751aad58
STFELIX999 <stevin.felix9@gmail.com>
2026-07-06 10:44:55 +0530

    feat: Authenticate requests using JWT SecurityContextHolder

--- src/main/java/com/stevin/personalfinancedashboard/security/JwtAuthenticationFilter.java
+++ src/main/java/com/stevin/personalfinancedashboard/security/JwtAuthenticationFilter.java
@@ -7,11 +7,27 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+
 import java.io.IOException;
 
 @Component
 public class JwtAuthenticationFilter
         extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public JwtAuthenticationFilter(
+            JwtService jwtService,
+            CustomUserDetailsService customUserDetailsService) {
+
+        this.jwtService = jwtService;
+        this.customUserDetailsService = customUserDetailsService;
+    }
 
     @Override
     protected void doFilterInternal(
@@ -50,9 +66,43 @@ public class JwtAuthenticationFilter
         // Extract JWT Token
         String jwt = authHeader.substring(7);
 
+        //Extract Email from JWT Token
+        String email = jwtService.extractUsername(jwt);
+        System.out.println("Email : " + email);
+
+        //Load User Details from Database using Email
+        UserDetails userDetails = customUserDetailsService
+                        .loadUserByUsername(email);
+
+        System.out.println(
+                userDetails.getUsername());
         System.out.println("JWT Token : " + jwt);
 
-        // Continue request
+        if(jwtService.isTokenValid(
+                jwt,
+                userDetails.getUsername())){
+
+            //Creating Authentication Object
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,
+                            userDetails.getAuthorities());
+            //Attaching Request Details to the Authentication Object
+            authentication.setDetails(
+                    new WebAuthenticationDetailsSource()
+                            .buildDetails(request));
+
+            //Storing Authentication Object in Security Context
+            SecurityContextHolder
+                    .getContext()
+                    .setAuthentication(authentication);
+
+            System.out.println("Authentication Stored Successfully");
+
+        }
+
+        // Continue request. Without this, the request will not proceed to the controller
         filterChain.doFilter(request, response);
 
     }
