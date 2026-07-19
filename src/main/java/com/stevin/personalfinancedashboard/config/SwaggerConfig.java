4ca74a8b70c120ae5c38deceddbde990c4fe1f9d
STFELIX999 <stevin.felix9@gmail.com>
2026-07-19 21:40:34 +0530

    feat: Integrated JWT authentication with Swagger UI

--- src/main/java/com/stevin/personalfinancedashboard/config/SwaggerConfig.java
+++ src/main/java/com/stevin/personalfinancedashboard/config/SwaggerConfig.java
@@ -5,10 +5,15 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 
 @Configuration
 public class SwaggerConfig {
 
+    private static final String SECURITY_SCHEME_NAME = "Bearer Authentication";
     @Bean
     public OpenAPI personalFinanceOpenAPI() {
 
@@ -23,6 +28,18 @@ public class SwaggerConfig {
                                                 .name("Stevin Felix")
                                                 .email("stevin.felix9@gmail.com")
                                 )
+                )
+                .components(new Components()
+                .addSecuritySchemes(
+                        SECURITY_SCHEME_NAME,
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ))
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList(SECURITY_SCHEME_NAME)
                 );
     }
 }
